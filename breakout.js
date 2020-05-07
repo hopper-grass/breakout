7d605736eaf663956a0faa215e97ff5028c48a42
hopper-grass <matthew.dyer@live.longwood.edu>
2020-05-07 12:53:47 -0400

    Added in collision detection

--- breakout.js
+++ breakout.js
@@ -6,12 +6,15 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
 
+//Adjust these to change ball speed
 var dx = 2;
 var dy = -2;
 
+var ballRadius = 10;
+
 function drawBall(){
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#D8BFD8";
 	ctx.fill();
 	ctx.closePath();
@@ -26,6 +29,18 @@ function draw(){
 	//Update locations
 	x += dx;
 	y += dy;
+
+	//Collision detection
+	var addX = x + dx;
+	var addY = y + dy;
+
+	if(addX > canvas.width-ballRadius || addX < ballRadius) {
+    	dx = -dx;
+	}
+
+	if(addY > canvas.height-ballRadius || addY < ballRadius) {
+    	dy = -dy;
+	}	
 }
 
 setInterval(draw, 10); //call draw every 10 milliseconds
