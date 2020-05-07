82059f3524b7f0badc600f26b7b929321dc2833d
hopper-grass <matthew.dyer@live.longwood.edu>
2020-05-07 13:12:33 -0400

    Added in the paddle and user input handlers

--- breakout.js
+++ breakout.js
@@ -1,16 +1,41 @@
-//Simple script to simulate a basic breakout-style game
+//Script to simulate a basic breakout-style game
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var x = canvas.width/2;
-var y = canvas.height-30;
-
-//Adjust these to change ball speed
+//Ball
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
-
 var ballRadius = 10;
+
+//Paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+//User Input
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e){
+	if(e.key == "Right" || e.key == "ArrowRight") {
+		rightPressed = true;
+	} else if(e.key == "Left" || e.key == "ArrowLeft") {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e){
+	if(e.key == "Right" || e.key == "ArrowRight") {
+		rightPressed = false;
+	} else if(e.key == "Left" || e.key == "ArrowLeft") {
+		leftPressed = false;
+	}
+}
 
 function drawBall(){
 	ctx.beginPath();
@@ -20,27 +45,45 @@ function drawBall(){
 	ctx.closePath();
 }
 
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "gray";
+	ctx.fill();
+	ctx.closePath();
+}
+
 function draw(){
 	//Clear out the frame
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	drawBall();
+	drawPaddle();
+
+	//Ball Collision detection
+	var addX = x + dx;
+	var addY = y + dy;
+
+	if(addX > canvas.width - ballRadius || addX < ballRadius) {
+    	dx = -dx;
+	}
+
+	if(addY > canvas.height - ballRadius || addY < ballRadius) {
+    	dy = -dy;
+	}
+
+	//Paddle Collision detection
+	if(rightPressed) {
+		paddleX += 7;
+		if(paddleX + paddleWidth > canvas.width) { paddleX = canvas.width - paddleWidth; }
+	} else if(leftPressed) {
+		paddleX -= 7;
+		if (paddleX < 0) { paddleX = 0; }
+	}
 
 	//Update locations
 	x += dx;
 	y += dy;
-
-	//Collision detection
-	var addX = x + dx;
-	var addY = y + dy;
-
-	if(addX > canvas.width-ballRadius || addX < ballRadius) {
-    	dx = -dx;
-	}
-
-	if(addY > canvas.height-ballRadius || addY < ballRadius) {
-    	dy = -dy;
-	}	
 }
 
 setInterval(draw, 10); //call draw every 10 milliseconds
